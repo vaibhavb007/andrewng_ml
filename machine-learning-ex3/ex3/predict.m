05f673ee061f478e75adfcaf818403bd6e03ce2a
vaibhav <vaibhavbhosale15@gmail.com>
2018-03-26 19:25:31 +0530

    week 4 completed

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -21,9 +21,9 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
-
-
-
+a = sigmoid([ones(size(X,1), 1) X] * Theta1');
+a = sigmoid([ones(size(a, 1), 1) a] * Theta2');
+[max_values, p] = max(a, [], 2);
 
 
 
